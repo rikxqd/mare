6e4769ac5e60d6481bf287b3770859faad12db09
muzuiget <muzuiget@gmail.com>
2017-03-10 16:36:01 +0800

    修改一下提示文本

--- lua-example/debug-test.lua
+++ lua-example/debug-test.lua
@@ -29,8 +29,17 @@ end)
 if config.start ~= false then
     debugger:start()
     if debugger.session:is_ready() then
-        local pwd = os.getenv('PWD')
-        local url = string.format('chrome-devtools://devtools/bundled/inspector.html?ws=127.0.0.1:9223/session/test?project.source=%s', pwd)
-        print(string.format('[mare] 此入口文件 Chrome 地址调试\n%s', url))
+        local dir = '/path/to/script_folder'
+        local builtin = 'chrome-devtools://devtools/bundled/inspector.html'
+        local custom = 'http://127.0.0.1:8001/devtools/inspector.html'
+        local ws_url = 'ws=127.0.0.1:9223/session/test'
+        local fmt = '* %s?%s?project.source=%s'
+        local url1 = fmt:format(builtin, ws_url, dir)
+        local url2 = fmt:format(custom, ws_url, dir)
+        print('[mare] You can debug this scirpt in Chrome:')
+        print(url1)
+        print(url2)
+    else
+        print('[mare] fail to connect server')
     end
 end
