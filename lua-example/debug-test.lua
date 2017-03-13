c29de69175ec1650ea7b55419b19fea158864b22
muzuiget <muzuiget@gmail.com>
2017-03-13 15:59:52 +0800

    修改一下调试地址提示信息

--- lua-example/debug-test.lua
+++ lua-example/debug-test.lua
@@ -28,18 +28,18 @@ end)
 
 if config.start ~= false then
     debugger:start()
+
+    local ci = debugger.config.iostream
+    local cs = debugger.config.session
+
     if debugger.session:is_ready() then
-        local dir = '/path/to/script_folder'
-        local builtin = 'chrome-devtools://devtools/bundled/inspector.html'
-        local custom = 'http://127.0.0.1:8001/devtools/inspector.html'
-        local ws_url = 'ws=127.0.0.1:9223/session/test'
-        local fmt = '* %s?%s?project.source=%s'
-        local url1 = fmt:format(builtin, ws_url, dir)
-        local url2 = fmt:format(custom, ws_url, dir)
-        print('[mare] You can debug this scirpt in Chrome:')
-        print(url1)
-        print(url2)
+        local dir = os.getenv('PWD') or 'example'
+        local path = 'http://%s:8001/devtools/inspector.html'
+        local args = '?ws=%s:9223/session/%s?project.source=%s'
+        local url = path:format(ci.host) .. args:format(ci.host, cs.id, dir)
+        print('[mare] devtools url: ' .. url)
     else
-        print('[mare] fail to connect server')
+        local server = string.format('%s:%s', ci.host, ci.port)
+        print('[mare] fail to connect debugger server: ' .. server)
     end
 end
